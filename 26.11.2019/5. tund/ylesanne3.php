fc9b0569213bc7eddd0966c133074db4d8dcdd66
Karel Kivimägi <karel.kivimagi@khk.ee>
2019-11-26 16:51:55 +0200

    26.11.2019 date and time ylesanne 3 metshein

--- 26.11.2019/5. tund/ylesanne3.php
+++ 26.11.2019/5. tund/ylesanne3.php
@@ -1,1 +1,27 @@
 <?php
+//kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta. ' ';
+
+$paevaNumber = date('N');
+$paev = array(
+    '1' => 'esmaspäev',
+    '2' => 'teisipäev',
+    '3' => 'kolmapäev',
+    '4' => 'neljapäev',
+    '5' => 'reede',
+    '6' => 'laupäev',
+    '7' => 'pühapäev',
+);
+foreach ($paev as $number=> $nimetus){
+    if($number == $paevaNumber){
+        echo $paev[$number];
+    }
+}
+echo '<hr>';
+?>
