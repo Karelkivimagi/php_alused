64c92450ed0eac2e0f90987dc6e5774841bf69f7
Karelkivimagi <54833613+Karelkivimagi@users.noreply.github.com>
2020-01-07 13:48:27 +0200

    Burger ül

--- 07.01.2020/burger.php
+++ 07.01.2020/burger.php
@@ -29,23 +29,26 @@ class Burger{
     public function MidaTeTellisite(){
 
         return  'Te tellisite: '.               $this -> nimetus.
-            '<br> milles on lihaks: ' .     $this -> liha.
-            '<br> milles on saiaks: ' .     $this -> sai.
-            '<br> milles on lisand 1:' .    $this -> lisand1.
-            '<br> milles on lisand 2:' .    $this -> lisand2.
-            '<br> milles on lisand 3:' .    $this -> lisand3.
-            '<br> milles on lisand 4:' .    $this -> lisand4
+            '<br> Liha: ' .     $this -> liha.
+            '<br> Sai: ' .     $this -> sai.
+            '<br> Lisand 1:' .    $this -> lisand1.
+            '<br> Lisand 2:' .    $this -> lisand2.
+            '<br> Lisand 3:' .    $this -> lisand3.
+            '<br> Lisand 4:' .    $this -> lisand4
             ;
     }
 }
 
 class TervislikBurger extends Burger{
 
-    public $sai = 'Tavaline sai';
-    public $tervislikLisand1 = 'juust';
+    public $sai = 'Must teraleib';
+    public $nimetus = 'Tervislik burger';
+    public $tervislikLisand1 = 'Lehtsalat';
     public $tervislikLisand1Hind = 0.20;
     public $tervislikLisand2 = 'Hapukurk';
-    public $tervislikLisand2Hind = 1;
+    public $tervislikLisand2Hind = 0.30;
+    public $lisand1 = 'sibul';
+    public $lisand1Hind = 0.20;
 
     public function BurgerSumma(){
         return
@@ -60,13 +63,13 @@ class TervislikBurger extends Burger{
     }
     public function MidaTeTellisite(){
         return  'Te tellisite: '.               $this -> nimetus.
-            '<br> milles on lihaks: ' .     $this -> liha.
-            '<br> milles on saiaks: ' .     $this -> sai.
-            '<br> milles on lisand 1:' .    $this -> lisand1.
-            '<br> milles on lisand 2:' .    $this -> lisand2.
-            '<br> milles on lisand 3:' .    $this -> lisand3.
-            '<br> milles on lisand 4:' .    $this -> lisand4.
-            '<br> Milles on tervislik lisand 1::' . $this -> tervislikLisand1.
+            '<br> Liha: ' .     $this -> liha.
+            '<br> Sai: ' .     $this -> sai.
+            '<br> Esimene lisand:' .    $this -> lisand1.
+            '<br> Teine lisand :' .    $this -> lisand2.
+            '<br> Kolmas lisand:' .    $this -> lisand3.
+            '<br> Neljas lisandlisand:' .    $this -> lisand4.
+            '<br> Milles on tervislik lisand 1:' . $this -> tervislikLisand1.
             '<br> Milles on tervislik lisand 2: ' . $this -> tervislikLisand2
             ;
     }
@@ -75,11 +78,51 @@ class TervislikBurger extends Burger{
 
 class LuxBurger extends burger{
 
+    public $sai = 'Teradega kukkel';
+    public $liha = 'Veiseliha';
+    public $nimetus = 'Luxburger';
+    public $tervislikLisand1 = 'Lehtsalat';
+    public $tervislikLisand1Hind = 0.20;
+    public $tervislikLisand2 = 'Hapukurk';
+    public $tervislikLisand2Hind = 0.30;
+    public $lisand1 = 'Sibul';
+    public $friikad = 'Friikartulid';
+    public $jook = 'Coca-Cola';
+    public $jookHind = 0.80;
+    public $friikadHind = 1.20;
+    public $lisand1Hind = 0.20;
+
+    public function BurgerSumma(){
+        return
+            $this -> hind +
+            $this -> lisand1Hind +
+            $this -> lisand2Hind +
+            $this -> lisand3Hind +
+            $this -> lisand4Hind +
+            $this -> tervislikLisand1Hind +
+            $this -> friikadHind +
+            $this -> jookHind +
+            $this -> tervislikLisand2Hind;
+
+    }
+    public function MidaTeTellisite(){
+        return  'Te tellisite: '.               $this -> nimetus.
+            '<br> Liha: ' .     $this -> liha.
+            '<br> Sai: ' .     $this -> sai.
+            '<br> Esimene lisand:' .    $this -> lisand1.
+            '<br> Teine lisand :' .    $this -> lisand2.
+            '<br> Kolmas lisand:' .    $this -> lisand3.
+            '<br> Muu:' . $this -> friikad.
+            '<br> Jook: ' . $this -> jook
+            ;
+    }
+
 }
 // anna burgeritele omadused
 $argiBurger = new burger();
 $kahekihine = new burger();
 $tavaTervislikBurger = new TervislikBurger();
+$tavaLuxBurger = new LuxBurger();
 
 $kahekihine -> nimetus = 'TopeltBurger';
 
@@ -92,14 +135,17 @@ $kahekihine -> lisand3Hind = '0.50';
 $kahekihine -> lisand4 = 'Lisa sai';
 $kahekihine -> lisand4Hind = '0.45';
 
-echo '<h1> tava burger </h1>';
+echo '<h1> Tellimus </h1>';
 echo $argiBurger -> MidaTeTellisite();
-echo '<br> See maksis:' . $argiBurger -> BurgerSumma();
-
-echo '<br> <h1>spetsiaalsete omadusetega tava burger</h1>';
+echo '<br> Kogu maksumus:' . $argiBurger -> BurgerSumma();
+echo '<br> <hr><br>';
 echo $kahekihine -> MidaTeTellisite();
-echo '<br> Mis maksis: '. $kahekihine -> BurgerSumma();
-
-
+echo '<br> Kogu maksumus: '. $kahekihine -> BurgerSumma();
+echo '<br> <hr><br>';
+echo $tavaTervislikBurger -> MidaTeTellisite();
+echo '<br> Kogu maksumus: '. $tavaTervislikBurger -> BurgerSumma();
+echo '<br> <hr><br>';
+echo $tavaLuxBurger -> MidaTeTellisite();
+echo '<br> Kogu maksumus: '. $tavaLuxBurger -> BurgerSumma();
 
 echo '<hr>';
